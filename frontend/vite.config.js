60823649ce50977356c060d00af93fbd33775d1e
Farhatul Hassan <farhatulhassan1@gmail.com>
2026-01-10 20:38:46 +0600

    changed proxy in vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [tailwindcss(), react()],
-    server: {
-        proxy: {
-            // Redirect all requests starting with '/api' to your Express server
-            "/api": {
-                target: import.meta.env.BACKEND_URL,
-                changeOrigin: true, // Needed for virtual hosted sites
-                rewrite: (path) => path.replace(/^\/api/, ""), // Optional: remove the /api prefix when forwarding to Express
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    console.log("Backend URL:", env.BACKEND_URL);
+    return {
+        plugins: [tailwindcss(), react()],
+        server: {
+            proxy: {
+                // // Redirect all requests starting with '/api' to your Express server
+                // "/api": {
+                //     target: env.BACKEND_URL,
+                //     changeOrigin: true, // Needed for virtual hosted sites
+                //     // rewrite: (path) => path.replace(/^\/api/, ""), // Optional: remove the /api prefix when forwarding to Express
+                // },
             },
         },
-    },
+    };
 });
